070797064e52b40012f7a73788be48590d97bf21
Olechka31 <Olechka31@users.noreply.github.com>
2022-10-23 13:10:20 +0200

    Edited convertation part

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -18,27 +18,27 @@ export default function WeatherTemperature(props) {
 
   if (unit === "celsius") {
     return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)} </span>
+      <span className="WeatherTemperature">
+        <span className="temperature"> {Math.round(props.celsius)} </span>
         <span className="unit">
           °C |{""}
           <a href="/" onClick={showFahrenheit}>
             °F
           </a>
         </span>
-      </div>
+      </span>
     );
   } else {
     return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit())} </span>
+      <span className="WeatherTemperature">
+        <span className="temperature"> {Math.round(fahrenheit())} </span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
             °C
           </a>
           {""}| °F
         </span>
-      </div>
+      </span>
     );
   }
 }
