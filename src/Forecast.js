8e12055e73540a0ec2d1caf7a572dc3c8a86af70
Olechka31 <Olechka31@users.noreply.github.com>
2022-12-05 12:41:23 +0100

    change some colors and margin

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ForecastDay from "./ForecastDay";
 
 export default function Forecast(props) {
